f6c6defd4bcc957597d9bbc773c77bfbc741de67
ra-f-sun <78753870+ra-f-sun@users.noreply.github.com>
2024-10-29 22:31:02 +0600

    added dataType and typeCasting file

---
+++ Getting_started/data_type/dataType.java
@@ -0,0 +1,10 @@
+public class dataType
+{
+    public static void main (String[] args)
+    {
+        int a=5;
+        int b=10;
+        int result=a+b;
+        System.out.println(result);
+    }
+}
---
+++ Getting_started/type_casting/typeCasting.java
@@ -0,0 +1,7 @@
+public class typeCasting
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Type casting");
+    }
+}
