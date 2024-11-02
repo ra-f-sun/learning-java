54eeae3feb8cdac22d6919507a158cbd62bb7ddf
ra-f-sun <78753870+ra-f-sun@users.noreply.github.com>
2024-11-03 02:19:04 +0600

    deleted some file

--- Getting_started/data_type/dataType.java
+++ Getting_started/data_type/dataType.java
@@ -1,10 +1,25 @@
-public class dataType
-{
-    public static void main (String[] args)
-    {
-        int a=5;
-        int b=10;
-        int result=a+b;
-        System.out.println(result);
+//In this file, we will apply the primitive data types used in java
+
+public class DataType {
+    public static void main(String[] args) {
+        // integer type data
+        byte a = 15;
+        short b = 30;
+        int c = 500;
+        long d = 1545454654;
+
+        // float type data
+        float e = 66.37f;
+        double f = 4545575454.35545;
+
+        // character type
+        char g = 'R';
+
+        // boolean type
+        boolean h = true;
+
+        // Printing all of them
+        System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g + " " + h);
+
     }
 }
