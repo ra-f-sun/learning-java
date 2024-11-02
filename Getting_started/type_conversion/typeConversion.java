72b970d332dd64fc59c90f33d1ca66f709bc1536
ra-f-sun <78753870+ra-f-sun@users.noreply.github.com>
2024-11-03 02:14:51 +0600

    Modified file names and added code to type conversion

---
+++ Getting_started/type_conversion/typeConversion.java
@@ -0,0 +1,37 @@
+//In this file we will try type conversions
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        // Let's take two variable among which we will perform conversion
+        int a = 5;
+        float b = 6.67f;
+        System.out.println("Before Conversion:");
+        System.out.println("Integer: " + a + " and Float: " + b);
+
+        // Performing integer to float conversion
+        float c = (float) a;
+        System.out.println("After performing integer to float: ");
+        System.out.println("Integer " + a + " Became " + c);
+
+        // Performing Float to integer conversion
+        int d = (int) b;
+        System.out.println("After performing float to integer: ");
+        System.out.println("Float " + b + " Became " + d + " And lost some values too");
+
+        // now for an interesting one that is converting to byte
+        byte e;
+        double f = 520.56;
+
+        e = (byte) f;
+        System.out.println("The double " + f + " Became " + e);
+
+    }
+}
+
+// If you see the value of e now, it is a bit weird
+// You know why?
+// Because when you convert some values into byte, it performs modules operation
+// The operation is "result=number%256"
+// Which means it divides the number with 256 and stores only the remainder
+// since "byte" stores integer type value, it tosses the fraction by default for
+// float or double
