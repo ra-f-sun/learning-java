04faf45498ce5e14d5766009ef3a76852eb44e6c
ra-f-sun <78753870+ra-f-sun@users.noreply.github.com>
2024-11-13 01:32:23 +0600

    Several files added

---
+++ array/OneDArray.java
@@ -0,0 +1,11 @@
+
+public class OneDArray {
+    public static void main(String[] args) {
+        int array[] = { 10, 65, 444, 2, 336, 31, 7, 555, 69, 3 };
+
+        System.out.println("The elements of the array are: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
